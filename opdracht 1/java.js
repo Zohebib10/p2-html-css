6fbab7134f6ac91084b2b23b5296599a28def9e0
Zohebib10 <9023992@student.zadkine.nl>
2023-11-16 15:28:57 +0100

    p2

---
+++ opdracht 1/java.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById("cookieWall");
+
+function showModal() {
+  modal.style.display = "block";
+
+}
+function removeModal() {
+modal.style.display = "none";
+
+}
+
+
+showModal()
